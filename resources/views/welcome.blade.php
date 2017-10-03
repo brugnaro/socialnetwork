c5e9531df3915c865e88acb6265a1899674776b1
marcos <marcos@marcosrodriguesbrugnaro@gmail.com>
2017-10-02 22:45:34 -0300

    mysql configuration and post test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,43 @@
 @extends('layouts.master')
 
 @section('title')
-	Welcome!
+  Welcome!
 @endsection
 
 @section('content')
-	<div class="row">
-		<div class="col-md-6">
-			<h3>Sign up</h3>
-			<form action="#" method="post">
-				<div class="form-group">
-					<label for="email">Your E-mail</label>
-					<input class="form-control" type="text" name="email" id="email">
-				</div>
-				<div class="form-group">
-					<label for="first_name">Your First Name</label>
-					<input class="form-control" type="text" name="first_name" id="first_name">
-				</div>
-				<div class="form-group">
-					<label for="password">Your Password</label>
-					<input class="form-control" type="password" name="password" id="password">
-				</div>
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</form>
-		</div>
-		<div class="col-md-6">
-			<h3>Sign in</h3>
-			<form action="#" method="post">
-				<div class="form-group">
-					<label for="email">Your E-mail</label>
-					<input class="form-control" type="text" name="email" id="email">
-				</div>
-				<div class="form-group">
-					<label for="password">Your Password</label>
-					<input class="form-control" type="password" name="password" id="password">
-				</div>
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</form>
-		</div>
-	</div>
+  <div class="row">
+    <div class="col-md-6">
+      <h3>Sign up</h3>
+      <form action="{{ route('signup') }}" method="post">
+        <div class="form-group">
+          <label for="email">Your E-mail</label>
+          <input class="form-control" type="text" name="email" id="email">
+        </div>
+        <div class="form-group">
+          <label for="first_name">Your First Name</label>
+          <input class="form-control" type="text" name="first_name" id="first_name">
+        </div>
+        <div class="form-group">
+          <label for="password">Your Password</label>
+          <input class="form-control" type="password" name="password" id="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <input type="hidden" name="_token" value="{{ Session::token() }}">
+      </form>
+    </div>
+    <div class="col-md-6">
+      <h3>Sign in</h3>
+      <form action="#" method="post">
+        <div class="form-group">
+          <label for="email">Your E-mail</label>
+          <input class="form-control" type="text" name="email" id="email">
+        </div>
+        <div class="form-group">
+          <label for="password">Your Password</label>
+          <input class="form-control" type="password" name="password" id="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
 @endsection
